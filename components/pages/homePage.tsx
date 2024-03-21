5ccf51c608d9e6cb4ea6258e03300a8dcc2a8fb2
juliscapucin <jscapucin@gmail.com>
2024-03-21 10:49:56 +0100

    showreel: fix image order

--- components/pages/homePage.tsx
+++ components/pages/homePage.tsx
@@ -3,7 +3,16 @@
 import { Showreel } from "@/components"
 import { Container } from "@/components/ui"
 
-const showreelImages = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"]
+const showreelImages = [
+	"01.jpg",
+	"02.jpg",
+	"03.jpg",
+	"04.jpg",
+	"05.jpg",
+	"06.jpg",
+	"07.jpg",
+	"08.jpg",
+]
 
 export default function HomePage() {
 	return (
