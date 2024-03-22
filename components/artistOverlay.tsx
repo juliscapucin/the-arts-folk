61986da67df3cb0226aa020a50c971f8c863e9be
juliscapucin <jscapucin@gmail.com>
2024-03-22 19:06:46 +0100

    artists-page: add artistsOverlay flex + responsiveness

--- components/artistOverlay.tsx
+++ components/artistOverlay.tsx
@@ -1,57 +1,128 @@
 import { CldImage } from "next-cloudinary"
 
-const artistOverlayImages = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"]
-
 export default function ArtistOverlay() {
 	return (
-		<div className='fixed w-full pr-8 max-w-desktop h-[--container-height-mobile] lg:h-[--container-height-desktop] flex justify-between z-10 pointer-events-none'>
-			{/* LEFT */}
-			<div className='w-[40%] flex gap-4'>
-				<div className='relative w-[50%]'>
-					<CldImage
-						className={`object-cover md:object-contain`}
-						src='the-arts-folk/01.jpg'
-						alt='photo'
-						sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw'
-						quality={50}
-						fill
-					/>
+		<>
+			{/* DESKTOP */}
+			<div className='hidden md:flex fixed w-full p-8 max-w-desktop h-[--container-height-mobile] lg:h-[--container-height-desktop] justify-between z-10 pointer-events-none'>
+				{/* LEFT */}
+				<div className='relative w-[40%] h-full flex gap-4'>
+					<div className='relative w-[50%] h-1/2'>
+						<CldImage
+							className={`object-contain`}
+							src='the-arts-folk/01.jpg'
+							alt='photo'
+							sizes='(max-width: 768px) 50vw, (max-width: 1200px) 50vw, 50vw'
+							quality={50}
+							fill
+						/>
+					</div>
+					<div className='relative w-[50%] h-1/2 self-center'>
+						<CldImage
+							className={`object-contain`}
+							src='the-arts-folk/02.jpg'
+							alt='photo'
+							sizes='(max-width: 768px) 50vw, (max-width: 1200px) 50vw, 50vw'
+							quality={50}
+							fill
+						/>
+					</div>
+					<div className='absolute w-[80%] lg:w-[50%] h-1/2 -bottom-24'>
+						<CldImage
+							className={`object-contain`}
+							src='the-arts-folk/horizontal.jpg'
+							alt='photo'
+							sizes='(max-width: 768px) 50vw, (max-width: 1200px) 50vw, 50vw'
+							quality={50}
+							fill
+						/>
+					</div>
 				</div>
-				<div className='relative w-[50%]'>
-					<CldImage
-						className={`object-cover md:object-contain`}
-						src='the-arts-folk/02.jpg'
-						alt='photo'
-						sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw'
-						quality={50}
-						fill
-					/>
-				</div>
-			</div>
 
-			{/* RIGHT */}
-			<div className='w-[40%] flex gap-4'>
-				<div className='relative w-[50%]'>
-					<CldImage
-						className={`object-cover md:object-contain`}
-						src='the-arts-folk/03.jpg'
-						alt='photo'
-						sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw'
-						quality={50}
-						fill
-					/>
-				</div>
-				<div className='relative w-[50%]'>
-					<CldImage
-						className={`object-cover md:object-contain`}
-						src='the-arts-folk/04.jpg'
-						alt='photo'
-						sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw'
-						quality={50}
-						fill
-					/>
+				{/* RIGHT */}
+				<div className='relative w-[40%] h-full flex flex-row gap-4'>
+					<div className='relative w-[50%] h-1/2'>
+						<CldImage
+							className={`object-contain`}
+							src='the-arts-folk/04.jpg'
+							alt='photo'
+							sizes='(max-width: 768px) 50vw, (max-width: 1200px) 50vw, 50vw'
+							quality={50}
+							fill
+						/>
+					</div>
+					<div className='relative w-[50%] h-1/2 self-end'>
+						<CldImage
+							className={`object-contain`}
+							src='the-arts-folk/05.jpg'
+							alt='photo'
+							sizes='(max-width: 768px) 50vw, (max-width: 1200px) 50vw, 50vw'
+							quality={50}
+							fill
+						/>
+					</div>
 				</div>
 			</div>
-		</div>
+			{/* MOBILE */}
+			<div className='flex md:hidden fixed w-full pr-8 max-w-desktop h-[--container-height-mobile] lg:h-[--container-height-desktop] justify-between z-10 pointer-events-none'>
+				{/* LEFT */}
+				<div className='relative w-[40%] h-full flex flex-col gap-4 '>
+					<div className='relative w-[80%] h-1/2 self-start'>
+						<CldImage
+							className={`object-contain`}
+							src='the-arts-folk/01.jpg'
+							alt='photo'
+							sizes='(max-width: 768px) 50vw, (max-width: 1200px) 50vw, 50vw'
+							quality={50}
+							fill
+						/>
+					</div>
+					<div className='relative w-[80%] h-1/2 self-end'>
+						<CldImage
+							className={`object-contain`}
+							src='the-arts-folk/02.jpg'
+							alt='photo'
+							sizes='(max-width: 768px) 50vw, (max-width: 1200px) 50vw, 50vw'
+							quality={50}
+							fill
+						/>
+					</div>
+					<div className='relative w-[100%] lg:w-[50%] h-1/2'>
+						<CldImage
+							className={`object-contain`}
+							src='the-arts-folk/horizontal.jpg'
+							alt='photo'
+							sizes='(max-width: 768px) 50vw, (max-width: 1200px) 50vw, 50vw'
+							quality={50}
+							fill
+						/>
+					</div>
+				</div>
+
+				{/* RIGHT */}
+				<div className='relative w-[40%] h-full flex flex-col gap-4'>
+					<div className='relative w-[80%] h-1/2'>
+						<CldImage
+							className={`object-contain`}
+							src='the-arts-folk/04.jpg'
+							alt='photo'
+							sizes='(max-width: 768px) 50vw, (max-width: 1200px) 50vw, 50vw'
+							quality={50}
+							fill
+						/>
+					</div>
+					<div className='relative w-[80%] h-1/2 self-end'>
+						<CldImage
+							className={`object-contain`}
+							src='the-arts-folk/05.jpg'
+							alt='photo'
+							sizes='(max-width: 768px) 50vw, (max-width: 1200px) 50vw, 50vw'
+							quality={50}
+							fill
+						/>
+					</div>
+				</div>
+			</div>
+		</>
 	)
 }
