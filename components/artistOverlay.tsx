2fb36cfaae3f46b74529e3d97bbf03c9d40986bf
juliscapucin <jscapucin@gmail.com>
2024-03-22 19:22:42 +0100

    artists-page: improve responsiveness

--- components/artistOverlay.tsx
+++ components/artistOverlay.tsx
@@ -4,10 +4,10 @@ export default function ArtistOverlay() {
 	return (
 		<>
 			{/* DESKTOP */}
-			<div className='hidden md:flex fixed w-full p-8 max-w-desktop h-[--container-height-mobile] lg:h-[--container-height-desktop] justify-between z-10 pointer-events-none'>
+			<div className='flex fixed w-full pr-8 lg:p-8 max-w-desktop h-[--container-height-mobile] lg:h-[--container-height-desktop] justify-between z-10 pointer-events-none'>
 				{/* LEFT */}
-				<div className='relative w-[40%] h-full flex gap-4'>
-					<div className='relative w-[50%] h-1/2'>
+				<div className='relative w-[45%] lg:w-[40%] h-full flex flex-col lg:flex-row gap-4'>
+					<div className='relative w-[80%] lg:w-[50%] h-1/2'>
 						<CldImage
 							className={`object-contain`}
 							src='the-arts-folk/01.jpg'
@@ -17,7 +17,7 @@ export default function ArtistOverlay() {
 							fill
 						/>
 					</div>
-					<div className='relative w-[50%] h-1/2 self-center'>
+					<div className='relative w-[80%] lg:w-[50%] h-1/2 self-end lg:self-center'>
 						<CldImage
 							className={`object-contain`}
 							src='the-arts-folk/02.jpg'
@@ -27,7 +27,7 @@ export default function ArtistOverlay() {
 							fill
 						/>
 					</div>
-					<div className='absolute w-[80%] lg:w-[50%] h-1/2 -bottom-24'>
+					<div className='relative lg:absolute w-[110%] lg:w-[70%] xl:w-[50%] h-1/2 lg:-bottom-24'>
 						<CldImage
 							className={`object-contain`}
 							src='the-arts-folk/horizontal.jpg'
@@ -40,8 +40,8 @@ export default function ArtistOverlay() {
 				</div>
 
 				{/* RIGHT */}
-				<div className='relative w-[40%] h-full flex flex-row gap-4'>
-					<div className='relative w-[50%] h-1/2'>
+				<div className='relative w-[45%] lg:w-[40%] h-full flex flex-col lg:flex-row gap-4'>
+					<div className='relative w-[80%] lg:w-[50%] h-1/2'>
 						<CldImage
 							className={`object-contain`}
 							src='the-arts-folk/04.jpg'
@@ -51,67 +51,7 @@ export default function ArtistOverlay() {
 							fill
 						/>
 					</div>
-					<div className='relative w-[50%] h-1/2 self-end'>
-						<CldImage
-							className={`object-contain`}
-							src='the-arts-folk/05.jpg'
-							alt='photo'
-							sizes='(max-width: 768px) 50vw, (max-width: 1200px) 50vw, 50vw'
-							quality={50}
-							fill
-						/>
-					</div>
-				</div>
-			</div>
-			{/* MOBILE */}
-			<div className='flex md:hidden fixed w-full pr-8 max-w-desktop h-[--container-height-mobile] lg:h-[--container-height-desktop] justify-between z-10 pointer-events-none'>
-				{/* LEFT */}
-				<div className='relative w-[40%] h-full flex flex-col gap-4 '>
-					<div className='relative w-[80%] h-1/2 self-start'>
-						<CldImage
-							className={`object-contain`}
-							src='the-arts-folk/01.jpg'
-							alt='photo'
-							sizes='(max-width: 768px) 50vw, (max-width: 1200px) 50vw, 50vw'
-							quality={50}
-							fill
-						/>
-					</div>
-					<div className='relative w-[80%] h-1/2 self-end'>
-						<CldImage
-							className={`object-contain`}
-							src='the-arts-folk/02.jpg'
-							alt='photo'
-							sizes='(max-width: 768px) 50vw, (max-width: 1200px) 50vw, 50vw'
-							quality={50}
-							fill
-						/>
-					</div>
-					<div className='relative w-[100%] lg:w-[50%] h-1/2'>
-						<CldImage
-							className={`object-contain`}
-							src='the-arts-folk/horizontal.jpg'
-							alt='photo'
-							sizes='(max-width: 768px) 50vw, (max-width: 1200px) 50vw, 50vw'
-							quality={50}
-							fill
-						/>
-					</div>
-				</div>
-
-				{/* RIGHT */}
-				<div className='relative w-[40%] h-full flex flex-col gap-4'>
-					<div className='relative w-[80%] h-1/2'>
-						<CldImage
-							className={`object-contain`}
-							src='the-arts-folk/04.jpg'
-							alt='photo'
-							sizes='(max-width: 768px) 50vw, (max-width: 1200px) 50vw, 50vw'
-							quality={50}
-							fill
-						/>
-					</div>
-					<div className='relative w-[80%] h-1/2 self-end'>
+					<div className='relative w-[80%] lg:w-[50%] h-1/2 self-end'>
 						<CldImage
 							className={`object-contain`}
 							src='the-arts-folk/05.jpg'
