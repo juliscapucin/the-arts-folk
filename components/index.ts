2e0a292a6e0a47a832e220cdbc9c25b3b1be856b
juliscapucin <jscapucin@gmail.com>
2024-03-27 12:57:37 +0100

    copyright bugfix

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,5 @@
 import ArtistOverlay from "./artistOverlay"
-import Copyright from "./copyright"
 import Intro from "./intro"
 import Showreel from "./showreel"
 
-export { ArtistOverlay, Copyright, Intro, Showreel }
+export { ArtistOverlay, Intro, Showreel }
