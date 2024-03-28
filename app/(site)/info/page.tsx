70a8cc10c9a1946f81a23f3b7fefaa3e1460503a
juliscapucin <jscapucin@gmail.com>
2024-03-28 09:27:27 +0100

    page-transitions: add force-dynamic to all pages

--- app/(site)/info/page.tsx
+++ app/(site)/info/page.tsx
@@ -3,6 +3,9 @@ import { notFound } from "next/navigation"
 import { InfoPage } from "@/components/pages"
 import { getInfoPage } from "@/sanity/sanity-queries"
 
+// Opt out of caching for all data requests in the route segment
+export const dynamic = "force-dynamic"
+
 export default async function Page() {
 	const infoData = await getInfoPage()
 
