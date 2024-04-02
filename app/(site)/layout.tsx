6be342c03e1ca7528da1c6ba79c3c2699a746f02
juliscapucin <jscapucin@gmail.com>
2024-04-02 10:58:35 +0200

    improvements/02-04: improve mobile transitions

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,11 +1,16 @@
 import type { Metadata } from "next"
 import localFont from "next/font/local"
 
-import { getPage } from "@/sanity/sanity-queries"
+import { getPage, getHeaderNavLinks } from "@/sanity/sanity-queries"
 
 import "../globals.css"
 import { Cookies, Header, Footer } from "@/components/ui"
 import { Intro } from "@/components"
+
+const fallbackNavLinks = [
+	{ title: "Artists", slug: "artists", order: 1 },
+	{ title: "Info", slug: "info", order: 2 },
+]
 
 export const metadata: Metadata = {
 	title: "The Arts Folk",
@@ -29,6 +34,9 @@ export default async function RootLayout({
 	children: React.ReactNode
 }>) {
 	const cookieData = await getPage("terms-and-privacy")
+	let navLinks = await getHeaderNavLinks()
+
+	if (!navLinks || navLinks.length === 0) navLinks = fallbackNavLinks
 
 	return (
 		<html lang='en' className='overflow-clip'>
@@ -36,7 +44,7 @@ export default async function RootLayout({
 				className={`${myFont.className} relative w-screen max-w-desktop min-h-svh mx-auto overflow-x-clip bg-white uppercase font-text font-thin`}
 			>
 				<Intro />
-				<Header />
+				<Header navLinks={navLinks} />
 				{children}
 				<Footer />
 				<Cookies cookieData={cookieData} />
