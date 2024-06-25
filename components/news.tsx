77174f8bda652f551808adbfa1a773cf4818702b
juliscapucin <jscapucin@gmail.com>
2024-06-25 16:38:11 +0200

    artist-page: fix button in news

--- components/news.tsx
+++ components/news.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import Link from "next/link"
 import { CldImage } from "next-cloudinary"
 import ReactPlayer from "react-player/vimeo"
 
@@ -34,50 +33,47 @@ export default function News({ news }: NewsProps) {
 						}`}
 						key={project.slug}
 					>
-						<Link href={`/news/${project.slug}`} passHref legacyBehavior>
-							<Button
-								classes={`relative overflow-clip flex flex-col ${
-									project.newsPageSize === "small"
-										? imageSizeSmall
-										: imageSizeBig
-								}`}
-							>
-								{/* Release Date */}
-								<p className='block font-script text-headlineLarge md:text-displayMedium text-center mx-auto'>
-									{project.releaseDate}
-								</p>
+						<Button
+							href={`/news/${project.slug}`}
+							classes={`relative overflow-clip flex flex-col ${
+								project.newsPageSize === "small" ? imageSizeSmall : imageSizeBig
+							}`}
+						>
+							{/* Release Date */}
+							<p className='block font-script text-headlineLarge md:text-displayMedium text-center mx-auto'>
+								{project.releaseDate}
+							</p>
 
-								{/* Project Image */}
-								{project.images[0].url.includes("vimeo") ? (
-									<ReactPlayer
-										url={project.images[0].url}
-										playing
-										playsinline
-										width='100%'
-										height='100%'
-										controls={false}
-										muted={true}
-										loop={true}
-									/>
-								) : (
-									<CldImage
-										className='object-contain'
-										src={project.images[0].url}
-										alt={`Photo by ${project.artistInfo?.name}`}
-										sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw'
-										quality={70}
-										width={project.images[0].width}
-										height={project.images[0].height}
-									/>
-								)}
+							{/* Project Image */}
+							{project.images[0].url.includes("vimeo") ? (
+								<ReactPlayer
+									url={project.images[0].url}
+									playing
+									playsinline
+									width='100%'
+									height='100%'
+									controls={false}
+									muted={true}
+									loop={true}
+								/>
+							) : (
+								<CldImage
+									className='object-contain'
+									src={project.images[0].url}
+									alt={`Photo by ${project.artistInfo?.name}`}
+									sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw'
+									quality={70}
+									width={project.images[0].width}
+									height={project.images[0].height}
+								/>
+							)}
 
-								{/* Project Info */}
-								<p className='block text-center mx-auto'>{project.title}</p>
-								<p className='block text-center mx-auto font-script text-headlineSmall md:text-headlineLarge capitalize'>
-									By {project.artistInfo?.name}
-								</p>
-							</Button>
-						</Link>
+							{/* Project Info */}
+							<p className='block text-center mx-auto'>{project.title}</p>
+							<p className='block text-center mx-auto font-script text-headlineSmall md:text-headlineLarge capitalize'>
+								By {project.artistInfo?.name}
+							</p>
+						</Button>
 					</article>
 				)
 			})}
