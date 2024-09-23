818d9a871203bc13ca57d7f74c4c123862ff596b
juliscapucin <jscapucin@gmail.com>
2024-09-23 11:59:39 +0200

    update CORS for cloudinary frame

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const ContentSecurityPolicy = `
   style-src 'self' 'unsafe-inline' use.typekit.net p.typekit.net;
   img-src 'self' data: blob: *.vimeocdn.com *.vimeo.com *.cloudinary.com res.cloudinary.com;
   media-src 'self' blob: *.vimeocdn.com *.vimeo.com *.cloudinary.com res.cloudinary.com;
-  frame-src 'self' player.vimeo.com *.vimeo.com;
+  frame-src 'self' player.vimeo.com *.vimeo.com https://console.cloudinary.com;
   connect-src 'self' vimeo.com *.vimeo.com *.vimeocdn.com *.cloudinary.com res.cloudinary.com fqgs6dmb.api.sanity.io wss://fqgs6dmb.api.sanity.io;
   font-src 'self' data: use.typekit.net;
   object-src 'none';
