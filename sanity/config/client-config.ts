1cc851da2adb93142d399abd9a36511947868dc9
juliscapucin <jscapucin@gmail.com>
2025-05-18 14:08:28 +0100

    fix env variables

--- sanity/config/client-config.ts
+++ sanity/config/client-config.ts
@@ -1,5 +1,5 @@
 const config = {
-	projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+	projectId: process.env.SANITY_PROJECT_ID!,
 	dataset: "production",
 	apiVersion: "2022-06-30",
 	useCdn: false,
